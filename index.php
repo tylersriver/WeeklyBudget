257895a45a7c482426a35330c411569ee75dbe54
tylersriver <tyler.w.sriver@eagles.oc.edu>
2018-08-11 13:50:23 -0500

    Minor reformatting stuff

--- index.php
+++ index.php
@@ -1,12 +1,15 @@
 <?php
 
 // PHP Includes
+// ---------------------------------------------------------------------
 require_once "php/models/SimpleSQL.php";
 require_once "php/models/SimpleORM.php";
 
-require_once('php/models/BudgetDB.php'); 
-require_once('php/lib/SimpleTable.php'); 
+require_once "php/models/BudgetDB.php"; 
+require_once "php/lib/SimpleTable.php"; 
 
+// Handle Routing
+// ---------------------------------------------------------------------
 if(isset($_GET['controller']) && isset($_GET['action']) ) {
     $controller = $_GET['controller'];
     $action = $_GET['action'];
@@ -14,5 +17,4 @@ if(isset($_GET['controller']) && isset($_GET['action']) ) {
     $controller = 'pages';
     $action = 'overview';
 }
-
 require_once('php/views/layout.php');
--- php/views/layout.php
+++ php/views/layout.php
@@ -1,19 +1,25 @@
 <DOCTYPE html>
 <html>
   <head>
+
+    <!-- CSS Includes
+    ========================================================================================================================================================================== -->
     <!-- Bootstrap -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <!-- Main Styles -->
     <link rel="stylesheet" href="styles/global-styles.css">
 
   </head>
+  
   <body>
+
+    <!-- Navigation Bar
+    ========================================================================================================================================================================== -->
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
         <a class="navbar-brand" href="#">Sriver Budget</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
-
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav mr-auto">
                 <li class="nav-item">
@@ -34,17 +40,22 @@
         </div>
     </nav>
 
-    <?php require_once('routes.php'); ?>
+    <!-- Route the action
+    ========================================================================================================================================================================== -->
+    <?php require_once('php/routes.php'); ?>
 
     <footer>
         <div class="footer"> Copyright Tyler Sriver | 2018 | <a href='https://github.com/tylersriver/WeeklyBudget'>GitHub Repo</a></div>
     </footer>
 
+    <!-- Javascript Includes
+    ========================================================================================================================================================================== -->
     <!-- JQuery -->
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <!-- Popper -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
     <!-- Boostrap JS -->
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+  
   <body>
 <html>
